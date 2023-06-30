b2797a378e7f52950696f4b751cf8b77575bc298
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2023-06-30 12:42:36 +0100

    build reinstall

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,8 @@
 // import { Form } from "react-router-dom";
 // import emailjs from "@email.s/browser";
 // import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "../index.css";
+import 'react-toastify/dist/ReactToastify.css';
+import '../index.css';
 
 const Contact = () => {
   // const form = useRef();
